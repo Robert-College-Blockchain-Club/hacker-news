7d7911bc8cc07e9d50a2ef5e1a77d829de2090f4
Thomas Sankara <thomassankara74@gmail.com>
2022-05-04 09:09:17 +0300

    update component

--- src/components/FetchNews.js
+++ src/components/FetchNews.js
@@ -5,8 +5,9 @@ import { format } from "date-fns"
 
 export default function FetchNews() {
   const [items, setItems] = useState([])
-  const [query, setQuery] = useState("")
+  const [query, setQuery] = useState("programming")
   const [text, setText] = useState("")
+  const [largeTitle, setLargeTitle] = useState([])
   const [isLoading, setIsLoading] = useState(true) // loading state
 
   useEffect(() => {
@@ -16,8 +17,12 @@ export default function FetchNews() {
       const url = `https://hn.algolia.com/api/v1/search?query=${query}`
       const res = await fetch(url)
       const data = await res.json()
-      data.hits.length = 10
+      // You can change the number of items you get back in your response using
+      // the `Array.length` method, as demonstrated below. Uncomment the line and
+      // reload your app to see it in action.
+      // data.hits.length = 10
       setItems(data.hits)
+      setLargeTitle(data.hits[0])
     }
 
     fetchNews()
@@ -39,74 +44,94 @@ export default function FetchNews() {
 
   return (
     <>
-      <main className="container-xl mx-auto">
-        <section className="h-96 bg-gradient-to-r from-yellow-500 via-red-500 to-yellow-600">
-          <h1 className="text-center uppercase text-4xl md:text-6xl font-bold text-white pt-20">
-            An all you can read buffet
-          </h1>
-        </section>
-
-        {/* Search form */}
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-wrap items-center justify-center px-5 -mt-32 mb-5"
-        >
-          <input
-            type="text"
-            name="text"
-            id="text"
-            placeholder="Search for something..."
-            autoComplete="off"
-            required
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            className="py-2 px-6 rounded shadow w-full md:w-1/2 bg-transparent border-2 border-white text-xl text-white placeholder-white lg:text-4xl"
-          />
-          <button
-            type="submit"
-            className="bg-white text-red-500 py-3 px-10 text-lg  mt-2 sm:mt-0 sm:ml-5 rounded shadow border-2 border-white-500 hover:bg-transparent transition-all duration-300 hover:text-white font-light tracking-wider uppercase"
-            onClick={handleSubmit}
-          >
-            Search
-          </button>
-        </form>
-        {/* End of search form */}
-
+      <main>
         {isLoading ? (
-          // The spinning thingy for the loading state. (Try to simulate a low-end mobile)
-          // Or a slow network to see how this works.
           <div className="spinner"></div>
         ) : (
-          <div className="p-5 grid grid-cols-1 gap-10 md:grid-cols-2 2xl:max-w-6xl 2xl:mx-auto">
-            {items.map((item) => {
-              const { author, created_at, objectID, title, url } = item
+          <>
+            {/* Search form */}
+            <form
+              onSubmit={handleSubmit}
+              className="flex place-items-center container mx-auto lg:max-w-4xl mt-10 px-5"
+            >
+              <input
+                type="text"
+                name="text"
+                id="text"
+                placeholder="Search for something..."
+                autoComplete="off"
+                required
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+                className="w-full py-2 px-4 rounded bg-transparent border border-gray-700 focus:border-gray-600 transition-all duration-150 outline-none text-gray-700 placeholder-gray-700 text-xl lg:text-4xl lg:pb-4 mr-5"
+              />
+              <button
+                type="submit"
+                onClick={handleSubmit}
+                className="bg-white border border-gray-700 text-xl lg:text-4xl py-2 px-6 rounded lg:pb-4 text-gray-700 hover:bg-transparent transition-all duration-150"
+              >
+                Search
+              </button>
+            </form>
+            {/* End of search form */}
 
-              return (
-                <article key={objectID} className="p-5 rounded shadow bg-white">
-                  <h3 className="font-bold text-gray-800 text-xl mb-3">
-                    {title}
-                  </h3>
-                  <p>
-                    Author: <em>{author}</em>
-                  </p>
-                  <button className="my-5">
-                    <a
-                      href={url}
-                      target="_blank"
-                      rel="noopenner noreferrer"
-                      className="bg-red-500 text-white py-2 px-6 rounded shadow border-2 border-red-500 hover:bg-transparent transition-all duration-300 hover:text-red-500 font-bold tracking-wide"
-                    >
-                      Read More
-                    </a>
-                  </button>
-                  <p>
-                    {/* Format date using the `format` method from `date-fns` */}
-                    Published: {format(new Date(created_at), "dd MMM yyyy")}
-                  </p>
-                </article>
-              )
-            })}
-          </div>
+            <article className="my-10 flex flex-col items-center justify-center container lg:max-w-4xl mx-auto px-5">
+              <h1 className="font-bold text-center text-4xl my-5 text-white lg:text-6xl">
+                {largeTitle.title}
+              </h1>
+              <a
+                href={largeTitle.url}
+                target="_blank"
+                rel="noreferrer"
+                className="border-b border-gray-700 text-gray-600 text-lg hover:text-gray-400 hover:border-gray-400"
+              >
+                Read Full Story
+              </a>
+            </article>
+
+            <article className="container mx-auto lg:max-w-4xl px-5">
+              <p className="text-gray-600">
+                Category:{" "}
+                <span className="font-bold text-gray-400 capitalize">
+                  {query}
+                </span>
+              </p>
+            </article>
+
+            <section className="grid grid-cols-1 gap-5 p-5 md:grid-cols-2 container mx-auto lg:max-w-4xl">
+              {items.map((item) => {
+                const { author, created_at, objectID, title, url } = item
+
+                return (
+                  <article
+                    key={objectID}
+                    className="bg-gray-800 rounded p-3 transition-all duration-150"
+                  >
+                    <h3 className="font-bold text-white text-lg mb-3">
+                      {title}
+                    </h3>
+                    <article className="flex items-center justify-between">
+                      <p className="text-gray-600">
+                        By <em>{author}</em>
+                      </p>
+                      <a
+                        href={url}
+                        target="_blank"
+                        rel="noopenner noreferrer"
+                        className="border-b border-gray-700 text-gray-600 text-lg hover:text-gray-400 hover:border-gray-400"
+                      >
+                        Read More
+                      </a>
+                    </article>
+                    <p className="text-gray-400 mt-10">
+                      {/* Format date using the `format` method from `date-fns` */}
+                      {format(new Date(created_at), "dd MMM yyyy")}
+                    </p>
+                  </article>
+                )
+              })}
+            </section>
+          </>
         )}
       </main>
     </>
